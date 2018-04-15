ecfffbdd9fe2cab06b47322d77cef7d85e2f19ff
João <joao.paulo.martins@gmail.com>
2018-04-15 17:35:05 +0100

    Add files via upload

--- DESede.java
+++ DESede.java
@@ -1,144 +1,92 @@
-package pt.isel.cc;
+package pt.security;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.Files;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.KeySpec;
-
-import javax.crypto.Cipher;
-import javax.crypto.CipherInputStream;
-import javax.crypto.CipherOutputStream;
-import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
+import java.security.*;
+import java.security.spec.*;
+import javax.crypto.*;
 import javax.crypto.spec.DESedeKeySpec;
 
 /**
- * @author jpmartins
- * 
- * Tipo que utiliza JCA para fazer a cifra, decifra e geração de chaves DESede para (e de) ficheiro.
+ * @author jpmartins Tipo que utiliza JCA para fazer a cifra, decifra e geração
+ *         de chaves DESede para (e de) ficheiro.
  */
 public class DESede {
 	/**
-	 * @param fileKeys - nome do ficheiro para onde será gerada a chave. 
+	 * @param fileKeys
+	 *            - nome do ficheiro para onde será gerada a chave.
 	 */
-	public void generateKey(String fileKeys) {
+	public void generateKey(String fileKeys) throws FileNotFoundException, IOException, NoSuchAlgorithmException {
 		System.out.println("generatingKey");
-		try (FileOutputStream fos= new FileOutputStream(fileKeys) ){ // Java 7 rules.	
+		try (FileOutputStream fos = new FileOutputStream(fileKeys)) {
 			SecretKey key = KeyGenerator.getInstance("DESede").generateKey();
-			// Gravar a chave no ficheiro.		
+			// Gravar a chave no ficheiro.
 			fos.write(key.getEncoded());
-		} catch (NoSuchAlgorithmException e1) {
-			e1.printStackTrace();
-			System.exit(-1);
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(-2);
 		}
 	}
-	
+
 	/**
-	 * @param fileIn - nome do ficheiro de onde será lida a mensagem a CIFRAR;
-	 * @param fileKeys - nome do ficheiro que contem a chave;
-	 * @param fileOut - nome do ficheiro para onde será escrito o conteudo do ficheiro fileIn CIFRADO.
+	 * @param fileIn
+	 *            - nome do ficheiro de onde será lida a mensagem a CIFRAR;
+	 * @param fileKeys
+	 *            - nome do ficheiro que contem a chave;
+	 * @param fileOut
+	 *            - nome do ficheiro para onde será escrito o conteudo do ficheiro
+	 *            fileIn CIFRADO.
 	 */
-	public void cifrar(String fileIn,String fileKeys,String fileOut) {	
+	public void cifrar(String fileIn, String fileKeys, String fileOut) throws FileNotFoundException, IOException,
+			InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException {
 		System.out.println("encrypt");
-		try (FileOutputStream fos= new FileOutputStream(fileOut) ){ // Java 7 rules.	
+		try (FileOutputStream fos = new FileOutputStream(fileOut)) {
 			// recuperar a chave do ficheiro
 			SecretKey key = getSecretKey(fileKeys);
 			// iniciar o "cifrador" e uma stream que permite cifrar os dados para o ficheiro
 			Cipher cip1 = Cipher.getInstance("DESede/ECB/NoPadding");
-			CipherOutputStream c_out = new CipherOutputStream(fos,cip1); // cifrado
-			cip1.init(Cipher.ENCRYPT_MODE,key);
-			// efectuar a cifra - Java 7 rules.
-			Files.copy(java.nio.file.Paths.get(fileIn), c_out);
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-			System.exit(-3);
-		} catch (NoSuchPaddingException e) {
-			e.printStackTrace();
-			System.exit(-4);
-		} catch (InvalidKeyException e) {
-			e.printStackTrace();
-			System.exit(-5);
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(-15);
-		} catch (InvalidKeySpecException e) {
-			e.printStackTrace();
-			System.exit(-18);
-		}
-	}
-	/**
-	 * @param fileIn - nome do ficheiro de onde será lida a mensagem a CIFRADA;
-	 * @param fileKeys - nome do ficheiro que contem a chave;
-	 * @param fileOut - nome do ficheiro para onde será escrito o conteudo do ficheiro fileIn DECIFRADO.
-	 */
-	public void decifra(String fileIn,String fileKeys,String fileOut) {
-		System.out.println("decrypt");
-		try {
-			// recuperar a chave do ficheiro
-			SecretKey key = getSecretKey(fileKeys);
-			// iniciar o "decifrador"
-			Cipher cip2 = Cipher.getInstance("DESede/ECB/NoPadding");
-			cip2.init(Cipher.DECRYPT_MODE,key);
-			CipherInputStream in = new CipherInputStream(new FileInputStream(fileIn),cip2); // le do ficheiro cifrado e decifra
-			// decifrar ficheiro, e grava o texto em claro no ficheiro fileOut
-			new File(fileOut).delete();
-			//Java 7 rules.
-			Files.copy(in,java.nio.file.Paths.get(fileOut));
-		} catch (NoSuchAlgorithmException e1) {
-			e1.printStackTrace();
-			System.exit(-6);
-		} catch (NoSuchPaddingException e1) {
-			e1.printStackTrace();
-			System.exit(-7);
-		} catch (InvalidKeyException e) {
-			e.printStackTrace();
-			System.exit(-9);
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(-16);
-		} catch (InvalidKeySpecException e) {
-			e.printStackTrace();
-			System.exit(-17);
-		}
-	}		
-
-	private SecretKey getSecretKey(String fileKeys) throws FileNotFoundException, IOException, InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException {
-		try (FileInputStream fis = new FileInputStream(fileKeys)){
-			int n = fis.available();
-	        byte[] k = new byte[n];
-	        fis.read(k);
-	        fis.close();
-	        KeySpec ks = new DESedeKeySpec(k);
-	        SecretKeyFactory kf  = SecretKeyFactory.getInstance("DESede");
-	        return kf.generateSecret(ks);
-		}
-	}
-	@SuppressWarnings("unused")
-	private void copiarStream(InputStream in, OutputStream c_out) {
-		// (*) Cifrar os dados do ficheiro input para o ficheiro output.
-		byte[] b = new byte[64];
-		try {
-			while(in.read(b) > 0) // (*) lê dados do ficheiro
-			{				 
-				c_out.write(b); // (*) faz update(b) cifrado parte dos dados lidos usando o Cipher, e escrevendo-os no ficheiro fileOut
+			try (CipherOutputStream c_out = new CipherOutputStream(fos, cip1)) { // cifrado
+				cip1.init(Cipher.ENCRYPT_MODE, key);
+				// efectuar a cifra - Java 7 rules.
+				Files.copy(java.nio.file.Paths.get(fileIn), c_out);
 			}
-			c_out.close(); // (*) faz dofinal() no Cipher
-		} catch (IOException e1) {
-			e1.printStackTrace();
-			System.exit(-13);
+		}
+	}
+
+	/**
+	 * @param fileIn
+	 *            - nome do ficheiro de onde será lida a mensagem a CIFRADA;
+	 * @param fileKeys
+	 *            - nome do ficheiro que contem a chave;
+	 * @param fileOut
+	 *            - nome do ficheiro para onde será escrito o conteudo do ficheiro
+	 *            fileIn DECIFRADO.
+	 */
+	public void decifra(String fileIn, String fileKeys, String fileOut)
+			throws InvalidKeyException, FileNotFoundException, NoSuchAlgorithmException, InvalidKeySpecException,
+			IOException, NoSuchPaddingException {
+		System.out.println("decrypt");
+		// recuperar a chave do ficheiro
+		SecretKey key = getSecretKey(fileKeys);
+		// iniciar o "decifrador"
+		Cipher cip2 = Cipher.getInstance("DESede/ECB/NoPadding");
+		cip2.init(Cipher.DECRYPT_MODE, key);
+		try (CipherInputStream in = new CipherInputStream(new FileInputStream(fileIn), cip2)) { // le do ficheiro
+																								// cifrado e decifra
+			// decifrar ficheiro, e grava o texto em claro no ficheiro fileOut
+			if (new File(fileOut).delete())
+				System.out.println("Deleted previous version of " + fileOut);
+			Files.copy(in, java.nio.file.Paths.get(fileOut));
+		}
+	}
+
+	private SecretKey getSecretKey(String fileKeys) throws FileNotFoundException, IOException, InvalidKeyException,
+			NoSuchAlgorithmException, InvalidKeySpecException {
+		try (FileInputStream fis = new FileInputStream(fileKeys)) {
+			int n = fis.available();
+			byte[] k = new byte[n];
+			fis.read(k);
+			fis.close();
+			KeySpec ks = new DESedeKeySpec(k);
+			SecretKeyFactory kf = SecretKeyFactory.getInstance("DESede");
+			return kf.generateSecret(ks);
 		}
 	}
 }
--- DESedeCLI.java
+++ DESedeCLI.java
@@ -1,4 +1,9 @@
-package pt.isel.cc;
+package pt.security;
+
+import java.io.IOException;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import javax.crypto.NoSuchPaddingException;
 
 /**
  * @author jpmartins
@@ -7,52 +12,58 @@ package pt.isel.cc;
 public final class DESedeCLI {
 
 	public static void main(String[] args) {
-		if (args.length >= 2) {
-			DESede des = new DESede();
-			String fileIn, fileKey, fileOut;
+		try {
+			if (args.length >= 2) {
+				DESede des = new DESede();
+				String fileIn, fileKey, fileOut;
 
-			if (args[0].equals("-g")) {
-				fileKey = (String) args[1];
-				des.generateKey(fileKey);
-			} else if (args[1].equals("-c")) {
-				if (args.length == 4) {
-					fileIn = (String) args[1];
-					fileKey = (String) args[2];
-					fileOut = (String) args[3];
-					des.cifrar(fileIn, fileKey, fileOut);
+				if (args[0].equals("-g")) {
+					fileKey = (String) args[1];
+					des.generateKey(fileKey);
+				} else if (args[0].equals("-c")) {
+					if (args.length == 4) {
+						fileIn = (String) args[1];
+						fileKey = (String) args[2];
+						fileOut = (String) args[3];
+						des.cifrar(fileIn, fileKey, fileOut);
+					} else {
+						showError("Número inválido de parametros");
+						System.exit(1);
+					}
+				} else if (args[0].equals("-d")) {
+					if (args.length == 4) {
+						fileIn = (String) args[1];
+						fileKey = (String) args[2];
+						fileOut = (String) args[3];
+						des.decifra(fileIn, fileKey, fileOut);
+					} else {
+						showError("Número inválido de parametros");
+						System.exit(2);
+					}
 				} else {
-					showError("NÃºmero invÃ¡lido de parametros");
-					System.exit(1);
+					showError(args[0]);
+					System.exit(3);
 				}
-			} else if (args[1].equals("-d")) {
-				if (args.length == 4) {
-					fileIn = (String) args[1];
-					fileKey = (String) args[2];
-					fileOut = (String) args[3];
-					des.decifra(fileIn, fileKey, fileOut);
-				} else {
-					showError("NÃºmero invÃ¡lido de parametros");
-					System.exit(2);
-				}
+				System.out.println("Operação terminada com sucesso.");
 			} else {
-				showError(args[0]);
-				System.exit(3);
+				showError(null);
+				System.exit(4);
 			}
-			System.out.println("OperaÃ§Ã£o terminada com sucesso.");
-		} else {
-			showError(null);
-			System.exit(4);
+		} catch (InvalidKeyException | NoSuchAlgorithmException | InvalidKeySpecException | NoSuchPaddingException
+				| IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
 		}
 	}
 
 	private static void showError(String string) {
-		System.out.print("Parametros invÃ¡lidos");
+		System.out.print("Parametros inválidos");
 		if (string != null)
 			System.out.print(": " + string);
-		System.out.println();//System.out.print(System.getProperty("line.separator"));
-		System.out.println("UtilizaÃ§Ã£o:");
+		System.out.println();
+		System.out.println("Utilização:");
 		System.out.println(" Gerar chaves: -g keyFileName");
-System.out.println(" Cifrar: -c fileToEncrypt keyFileName newEncryptFileName");
+		System.out.println(" Cifrar: -c fileToEncrypt keyFileName newEncryptFileName");
 		System.out.println(" Decifrar: -d encryptFileName keyFileName newDecryptfileName");
 	}
 
