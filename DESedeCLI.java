df63619682aa939650bbefdd1a3023c088a303f0
João <joao.paulo.martins@gmail.com>
2018-04-15 16:40:02 +0100

    Update DESedeCLI.java

--- DESedeCLI.java
+++ DESedeCLI.java
@@ -7,38 +7,38 @@ package pt.isel.cc;
 public final class DESedeCLI {
 
 	public static void main(String[] args) {
-		if (args.length >= 3) {
+		if (args.length >= 2) {
 			DESede des = new DESede();
 			String fileIn, fileKey, fileOut;
 
-			if (args[1].equals("-g")) {
-				fileKey = (String) args[2];
+			if (args[0].equals("-g")) {
+				fileKey = (String) args[1];
 				des.generateKey(fileKey);
 			} else if (args[1].equals("-c")) {
-				if (args.length == 5) {
-					fileIn = (String) args[2];
-					fileKey = (String) args[3];
-					fileOut = (String) args[4];
+				if (args.length == 4) {
+					fileIn = (String) args[1];
+					fileKey = (String) args[2];
+					fileOut = (String) args[3];
 					des.cifrar(fileIn, fileKey, fileOut);
 				} else {
-					showError("Número inválido de parametros");
+					showError("NÃºmero invÃ¡lido de parametros");
 					System.exit(1);
 				}
 			} else if (args[1].equals("-d")) {
-				if (args.length == 5) {
-					fileIn = (String) args[2];
-					fileKey = (String) args[3];
-					fileOut = (String) args[4];
+				if (args.length == 4) {
+					fileIn = (String) args[1];
+					fileKey = (String) args[2];
+					fileOut = (String) args[3];
 					des.decifra(fileIn, fileKey, fileOut);
 				} else {
-					showError("Número inválido de parametros");
+					showError("NÃºmero invÃ¡lido de parametros");
 					System.exit(2);
 				}
 			} else {
-				showError(args[1]);
+				showError(args[0]);
 				System.exit(3);
 			}
-			System.out.println("Operação terminada com sucesso.");
+			System.out.println("OperaÃ§Ã£o terminada com sucesso.");
 		} else {
 			showError(null);
 			System.exit(4);
@@ -46,13 +46,13 @@ public final class DESedeCLI {
 	}
 
 	private static void showError(String string) {
-		System.out.print("Parametros inválidos");
+		System.out.print("Parametros invÃ¡lidos");
 		if (string != null)
 			System.out.print(": " + string);
 		System.out.println();//System.out.print(System.getProperty("line.separator"));
-		System.out.println("Utilização:");
+		System.out.println("UtilizaÃ§Ã£o:");
 		System.out.println(" Gerar chaves: -g keyFileName");
-		System.out.println(" Cifrar: -c fileToEncrypt keyFileName newEncryptFileName");
+System.out.println(" Cifrar: -c fileToEncrypt keyFileName newEncryptFileName");
 		System.out.println(" Decifrar: -d encryptFileName keyFileName newDecryptfileName");
 	}
 
